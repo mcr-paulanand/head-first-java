fc5e3cd74c49fc7c21bce693a1d5baba492b6430
Paul Anand <paul.anand@mcruncher.com>
2023-09-21 17:37:46 +0800

    Modified classes as per chap 6

--- chapter-5/simple-startup/SimpleStartupTestDrive.java
+++ chapter-5/simple-startup/SimpleStartupTestDrive.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 public class SimpleStartupTestDrive {
   public static void main(String[] args) {
     SimpleStartup dot = new SimpleStartup();
@@ -19,25 +21,28 @@ public class SimpleStartupTestDrive {
 }
 
 class SimpleStartup {
-  private int[] locationCells;
+  private ArrayList<String> locationCells;
   private int numOfHits = 0;
 
-  public void setLocationCells(int[] locs) {
+  public void setLocationCells(ArrayList<String> locs) {
     locationCells = locs;
   }
 
-  public String checkYourself(int guess) {
+  public String checkYourself(String userInput) {
     String result = "miss";
-    for (int cell : locationCells) {
-      if (guess == cell) {
+
+    int index = locationCells.indexOf(userInput);
+
+    if (index >= 0) {
+      locationCells.remove(index);
+
+      if (locationCells.isEmpty()) {
+        result = "kill";
+      } else {
         result = "hit";
-        numOfHits++;
-        break;
       }
+
     }
-    if (numOfHits == locationCells.length) {
-      result = "kill";
-      System.out.println(result);
-      return result;
-    }
+    return result;
   }
+}
