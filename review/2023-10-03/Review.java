62c99434dffea5af1505978afd0ce8607125193a
Paul Anand <paul.anand@mcruncher.com>
2023-10-05 11:17:24 +0800

    Added code on Object equals and hashCode

--- review/2023-10-03/Review.java
+++ review/2023-10-03/Review.java
@@ -1,30 +1,44 @@
 class Review {
     public static void main(String[] args) {
-        Terminal t1 = new Terminal();
-        System.out.println(t1.ipAddress + " " + t1.serialNumber);
+        System.out.println("TERMINAL\n========");
+        Terminal t1 = new Terminal("2.2.2.2", "0987654321");
+        System.out.println("t1: " + t1.ipAddress + " " + t1.serialNumber);
         Terminal t2 = new Terminal("2.2.2.2", "0987654321");
-        System.out.println(t2.ipAddress + " " + t2.serialNumber);
-        Terminal t3 = new Terminal("2.2.2.2", "0987654321");
-        System.out.println(t3.ipAddress + " " + t3.serialNumber);
-        System.out.println("Is t2 and t3 equals: " + t2.equals(t3));
-        System.out.println("t2 hashcode: " + t2.hashCode());
-        System.out.println("t3 hashcode: " + t3.hashCode());
+        System.out.println("t2: " + t2.ipAddress + " " + t2.serialNumber);
+        System.out.println();
 
+        System.out.println("Is t1 and t2 equals: " + t1.equals(t2));
+        System.out.println("t1 hashcode: " + t1.hashCode());
+        System.out.println("t2 hashcode: " + t2.hashCode());
+        System.out.println();
+
+        System.out.println("COMPUTER\n========");
         Computer c1 = new Computer();
         c1.setIpAddress("5.5.5.5");
         c1.setSerialNumber("A910J7T42P");
-        System.out.println(c1.getIpAddress() + " " + c1.getSerialNumber());
+        System.out.println("c1: " + c1.getIpAddress() + " " + c1.getSerialNumber());
+        Computer c2 = new Computer();
+        c2.setIpAddress("5.5.5.5");
+        c2.setSerialNumber("A910J7T42P");
+        System.out.println("c2: " + c2.getIpAddress() + " " + c2.getSerialNumber());
+        System.out.println();
+
+        System.out.println("Is c1 and c2 equals: " + c1.equals(c2));
+        System.out.println("c1 hashcode: " + c1.hashCode());
+        System.out.println("c2 hashcode: " + c2.hashCode());
+        System.out.println();
+
+        Computer c3 = c2;
+        System.out.println("Is c2 and c3 equals: " + c2.equals(c3));
+        System.out.println("c2 hashcode: " + c2.hashCode());
+        System.out.println("c3 hashcode: " + c3.hashCode());
+        System.out.println();
     }
 }
 
 class Terminal {
     String ipAddress;
     String serialNumber;
-
-    Terminal() {
-        this.ipAddress = ipAddress;
-        this.serialNumber = serialNumber;
-    }
 
     Terminal(String ipAddress, String serialNumber) {
         this.ipAddress = ipAddress;
@@ -47,15 +61,15 @@ class Computer {
         return this.serialNumber;
     }
 
-    void setIpAddress(String userInput) {
-        if (userInput.matches(IPV4_REGEX_PATTERN)) {
-            ipAddress = userInput;
+    void setIpAddress(String ipAddress) {
+        if (ipAddress.matches(IPV4_REGEX_PATTERN)) {
+            this.ipAddress = ipAddress;
         }
     }
 
-    void setSerialNumber(String userInput) {
-        if (userInput.matches(SERIAL_NUMBER_REGEX_PATTERN)) {
-            serialNumber = userInput;
+    void setSerialNumber(String serialNumber) {
+        if (serialNumber.matches(SERIAL_NUMBER_REGEX_PATTERN)) {
+            this.serialNumber = serialNumber;
         }
     }
 }
